4928f938c2f03ce9f38b0126b5e5e37e66b9ea15
Priyankjoshi-github <102507819+priyankjoshi-github@users.noreply.github.com>
2022-05-17 02:18:34 +0530

    Add files via upload

---
+++ Javascript Object/PersonObject.js
@@ -0,0 +1,6 @@
+//object with firstName and lastName properties
+var Person = {
+    firstName : "Priyank",
+    lastName : "Joshi"
+};
+console.log(Person);
---
+++ Javascript Object/ReadMiddleNameProperty.js
@@ -0,0 +1,8 @@
+//reading the middleName property from the variable.
+
+var Person = {
+  firstName: "Priyank",
+  middleName: "Rahul",
+  lastName: "Joshi",
+};
+console.log(Person.middleName);
